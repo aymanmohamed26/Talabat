113da7bea6b17d00ad972349447746abed8cddd5
Ayman_Mohamed <ayman0106210@gmail.com>
2024-07-06 01:40:31 +0300

    commit11Initial commit

---
+++ Talabat.G1.Soulution/TalabatApi/Dtos/CustomerBasketDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TalabatApi.Dtos
+{
+    public class CustomerBasketDto
+    {
+        [Required]
+        public string Id { get; set; }
+        public List <BasketItemDto> Items { get; set; }
+
+        public string? PaymentIntentId { get; set; }//todo edit here
+        public string? ClientSecret { get; set; } //todo edit here
+        public int? DeliveryMethoudId { get; set; }
+        public Decimal ShippingCost { get; set; }
+    }
+}
